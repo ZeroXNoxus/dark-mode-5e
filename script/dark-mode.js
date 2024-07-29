7e5eed4999425d05a5160b1907ca165d4ff10918
Nikolai Kucksdorf <nikolai.kucksdorf@live.de>
2024-07-29 21:42:47 +0200

    Update

--- script/dark-mode.js
+++ script/dark-mode.js
@@ -1,23 +1,8 @@
 function changeMode(val, name){
-    if(val == true){
-        $('body.vtt').addClass(name);
-    } else {
-        $('body.vtt').removeClass(name);
-    }
+    (val == true) ? $('body.vtt').addClass(name) : $('body.vtt').removeClass(name);
 }
 Hooks.once('init', () => {
-    game.settings.register('zetas-dark-mode', 'enabled', {
-        name: 'Enable Dark Mode',
-        hint: "Enable dark mode for DnD 5th Edition.",
-        scope: 'client',
-        config: true,
-        type: Boolean,
-        default: false,
-        onChange: val => {
-            changeMode(val, 'zetas-dark-mode');
-        }
-    });
-    game.settings.register('zetas-dark-mode', 'dir-changes', {
+    game.settings.register('dark-mode-5e', 'dir-changes', {
         name: 'Toggle Sidetab Changes',
         hint: "Enable this setting to make the right-hand sidetab more compact and modern.",
         scope: 'client',
@@ -26,8 +11,8 @@ Hooks.once('init', () => {
         default: true,
         onChange: val => {
             changeMode(val, 'zetas-dark-mode-dir');
-        }
+        },
+        requiresReload: true
     });
-    changeMode(game.settings.get('zetas-dark-mode', 'enabled'), 'zetas-dark-mode');
-    changeMode(game.settings.get('zetas-dark-mode', 'dir-changes'), 'zetas-dark-mode-dir');
+    changeMode(game.settings.get('dark-mode-5e', 'dir-changes'), 'zetas-dark-mode-dir');
 });
